b4956481e02d062400f29e390b93e58395a5274b
Isabelle <91973420+isabdch@users.noreply.github.com>
2022-05-13 17:39:28 -0300

    update

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Main from "./components/Main/Main.vue";
+</script>
+
+<template>
+  <Main />
+</template>
+
+<style></style>
---
+++ src/components/Main/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <h1>Olá</h1>
+  </main>
+</template>
+
+<script lang="ts">
+import { api } from "@/api/api";
+
+export default {
+  name: "Main",
+  data() {
+      return {
+
+      }
+  },
+  mounted() {
+    // setInterval(async () => {
+      api
+        .get("simple/price?ids=bitcoin&vs_currencies=brl")
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log(err));
+    // }, 2000);
+  },
+};
+</script>
+
+<style lang="scss"></style>
